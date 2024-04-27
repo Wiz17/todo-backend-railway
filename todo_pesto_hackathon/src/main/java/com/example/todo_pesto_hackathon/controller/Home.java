46fcdaa420d2bc13a91508da55638b399b985d7d
Saif Metkari <88455816+Wiz17@users.noreply.github.com>
2024-04-27 21:44:57 +0530

    Update Home.java

--- todo_pesto_hackathon/src/main/java/com/example/todo_pesto_hackathon/controller/Home.java
+++ todo_pesto_hackathon/src/main/java/com/example/todo_pesto_hackathon/controller/Home.java
@@ -21,6 +21,7 @@ import com.example.todo_pesto_hackathon.service.UserService;
 
 @RestController
 @RequestMapping("/home")
+@CrossOrigin(origins = "http://localhost:3000")
 public class Home {
 
 	@Autowired
